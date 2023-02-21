ed4a5098c44c1c1514fba46b451be149d1a3bbd3
ralcaraz <git@ricardoalcaraz.dev>
2023-02-21 12:45:38 +0000

    Added more setup

--- scripts/setup-script.sh
+++ scripts/setup-script.sh
@@ -2,3 +2,7 @@ pacman -Syu --no-confirm dotnet-sdk dotnet-runtime aspnet-runtime htop neofetch 
 usermod -s /usr/bin/zsh root
 git config --global user.email "git@ricardoalcaraz.dev"
 git config --global user.name "ralcaraz"
+echo $SSH__KEY > /root/.ssh/id_rsa
+echo $SSH__KEY__PUBLIC > /root/.ssh/id_rsa.pub
+chmod 400 /root/.ssh/id_rsa
+chmod 444 /root/.ssh/id_rsa.pub
